387e055a481086e79c079abf3f206bd121627d6e
d1msk1y <exect81@gmail.com>
2024-11-07 09:55:17 +0100

    Implement functional xml upload
    Read the XML with fs

--- examples/nodejs/src/index.js
+++ examples/nodejs/src/index.js
@@ -1,15 +1,25 @@
-import axios from 'axios';
+﻿import axios from 'axios';
+import fs from 'fs';
+import FormData from 'form-data';
+
+const filePath = './xml/SGr_00_0016_dddd_Siemens_PAC2200_ModbusTCP_V0.1 - full.xml';
+
+const data = new FormData();
+data.append('file', fs.createReadStream(filePath));
+data.append('fileName', 'SGr_00_0016_dddd_Siemens_PAC2200_ModbusTCP_V0.1 - full.xml');
 
 const requestConfig = {
-  method: 'POST',
+  method: 'post',
+  maxBodyLength: Infinity,
   url: 'http://localhost:8080/eiXml/SGr_00_0016_dddd_Siemens_PAC2200_ModbusTCP_V0.1 - full.xml',
   headers: {
-    'Content-Type': 'application/json'
+    ...data.getHeaders()
   },
-  data: {
-    file: '../xml/SGr_00_0016_dddd_Siemens_PAC2200_ModbusTCP_V0.1 - full.xml',
-    fileName: 'SGr_00_0016_dddd_Siemens_PAC2200_ModbusTCP_V0.1 - full.xml'
-  }
+  data: data
 };
 
-axios(requestConfig)
+axios.request(requestConfig).then((response) => {
+  console.log("Response: ", response.data);
+}).catch((error) => {
+  console.error("Error: ", error);
+});
