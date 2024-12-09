de706b38478b3ed51023a3ff108647e6b2cf5ddb
Sandro Sabatini <ssabatini@ergonomics.ch>
2024-12-09 11:22:42 +0100

    added LICENSE.md and a copyright notice referring to this LICENSE.md in every source file

--- src/main/java/com/smartgridready/intermediary/entity/Device.java
+++ src/main/java/com/smartgridready/intermediary/entity/Device.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2024 Verein SmartGridready Switzerland
+ *
+ * This file is licensed under the terms in LICENSE.md at the root of this project.
+ */
 package com.smartgridready.intermediary.entity;
 
 import java.util.ArrayList;
