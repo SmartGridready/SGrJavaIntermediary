3c1282562cf33a0a4edcb9af71239a5ae733c7a6
Matthias Krebs <matthias.krebs@ateleris.ch>
2025-04-10 19:34:01 +0200

    implemented introspection similar to test lab.

---
+++ src/main/java/com/smartgridready/intermediary/dto/FunctionalProfileDto.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright(c) 2025 Verein SmartGridready Switzerland
+ *
+ * This file is licensed under the terms in LICENSE.md at the root of this project.
+ */
+package com.smartgridready.intermediary.dto;
+
+import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Represents a functional profile.")
+public record FunctionalProfileDto(
+    @Schema(description = "The functional profile name, defined by EID")
+    String functionalProfileName,
+
+    @Schema(description = "The functional profile type, defined by SGr standard")
+    String functionalProfileType,
+
+    @Schema(description = "The functional profile category, defined by SGr standard")
+    String functionalProfileCategory,
+
+    @Schema(description = "The data points, defined by EID")
+    List<DataPointDto> dataPoints,
+
+    @Schema(description = "The generic attributes, defined by EID")
+    List<GenericAttributeDto> genericAttributes
+) {}
